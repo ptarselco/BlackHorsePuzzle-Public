df912f038a72ed6e36b77921e9c00d6be2cb8da5
ptarselco <ptasm@arselco.com>
2025-06-15 10:43:55 +0700

    Rev. Level02Scene - Running Black Horse Puzzle

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ window.onload = function () {
     width: 1920,
     height: 1280,
     //backgroundColor: "#000000",
-    scene: [SplashScene.min, Level01Scene.min, Level02Scene.min],
+    scene: [SplashScene, Level01Scene, Level02Scene],
   };
 
   const game = new Phaser.Game(config);
