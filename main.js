c1c335159ab9de86b2b698160da7c74cc245ee49
ptarselco <ptasm@arselco.com>
2025-06-16 22:20:13 +0700

    atur zoom dan BG level02

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 // main.js (Non-module version with CDN)
+import SplashScene from './Scenes/SplashScene.js';
+import Level01Scene from './Scenes/Level01Scene.js';
+import Level02Scene from './Scenes/Level02Scene.js';
 
 window.onload = function () {
   const config = {
     type: Phaser.AUTO,
-     width: 1920,
-     height: 1280,
+     //width: 1920,
+     //height: 1280,
      backgroundColor: "#181c24", // 🌟 warna latar belakang kebiruan
      parent: 'game-container', // opsional
-    scale: {
+     width: 1920,
+     height:1280,
+    
+     scale: {
       mode: Phaser.Scale.FIT, // 🌟 agar responsive
       autoCenter: Phaser.Scale.CENTER_BOTH, // 🌟 agar muncul di tengah
-    width: 1920,
-    height:1280,
     },
 
     pixelArt: true, // 🌟 agar grafik tetap tajam saat diskalakan
@@ -24,6 +28,7 @@ window.onload = function () {
   }
 };
 const game = new Phaser.Game(config);
+window.game = game;
 };
 
 
