906203e6550f4b7ba2427793c326d046d76087a0
ptarselco <ptasm@arselco.com>
2025-06-17 11:54:53 +0700

    Update main.js

--- main.js
+++ main.js
@@ -8,11 +8,18 @@ window.onload = function () {
      backgroundColor: "#181c24", // 🌟 warna latar belakang kebiruan
      parent: 'game-container', // opsional
     
-    
-  scale: {
-  mode: Phaser.Scale.RESIZE, // 🌟 agar layar fleksibel & tidak hardcoded
-  autoCenter: Phaser.Scale.CENTER_BOTH
-  },
+     scale: {
+      mode: Phaser.Scale.FIT, // 🌟 agar responsive
+      autoCenter: Phaser.Scale.CENTER_BOTH, // 🌟 agar muncul di tengah
+      width: 1920,
+      height:1280,
+   },
+
+
+  //scale: {
+  //mode: Phaser.Scale.RESIZE, // 🌟 agar layar fleksibel & tidak hardcoded
+  //autoCenter: Phaser.Scale.CENTER_BOTH
+ // },
 
     pixelArt: true, // 🌟 agar grafik tetap tajam saat diskalakan
     scene: [SplashScene, Level01Scene, Level02Scene],
