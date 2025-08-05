da7cf12e75d3a73bb28a747c7776a70c6d40de2e
ptarselco <ptasm@arselco.com>
2025-08-05 23:27:48 +0700

    Update Level01Scene.js

--- Scenes/Level01Scene.js
+++ Scenes/Level01Scene.js
@@ -3586,8 +3586,8 @@ showHoldMessageAboveNotes() {
               localStorage.removeItem(`gameOver_${email}`);
               alert('Level successfully unlocked. Enjoy playing again!');
               this.unblur10PuzzleButton();
-              //this.unlockGameAfterPurchase();
-              window.unlockPlayAndHideGameOver && window.unlockPlayAndHideGameOver();
+              this.unlockGameAfterPurchase();
+              //window.unlockPlayAndHideGameOver && window.unlockPlayAndHideGameOver();
               location.reload();
               }
             }
@@ -3797,8 +3797,8 @@ showHoldMessageAboveNotes() {
               localStorage.removeItem(`gameOver_${email}`);
               alert('Level successfully unlocked. Enjoy playing again!');
               this.unblur10PuzzleButton();
-              //this.unlockGameAfterPurchase();
-              window.unlockPlayAndHideGameOver && window.unlockPlayAndHideGameOver();
+              this.unlockGameAfterPurchase();
+              //window.unlockPlayAndHideGameOver && window.unlockPlayAndHideGameOver();
               location.reload();
               }   
             }
