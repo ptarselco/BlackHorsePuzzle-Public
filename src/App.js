74f776bf9f8092fe80fdb658243a07a535abaa32
ptarselco <ptasm@arselco.com>
2025-08-23 21:43:27 +0700

    Restructure files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+function App() {
+  return (
+    <div>
+      <h1>Black Horse Puzzle</h1>
+      {/* Kamu bisa tambahkan komponen/game kamu di sini */}
+    </div>
+  );
+}
+
+export default App;
